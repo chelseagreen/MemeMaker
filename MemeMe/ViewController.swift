49e334934050248aecf41d11eb4ca3d4e0bb5c86
Chelsea Green <chelseagreen@chelseas-mbp.gateway.pace.com>
2015-12-20 19:09:51 -0800

    updates

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,18 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate {
+UINavigationControllerDelegate, UITextFieldDelegate {
+    
     //Use this in the two delegate methods to pick cancel
     //self.dismissViewControllerAnimated(true, completion: nil)
 
     @IBOutlet weak var imageViewPicker: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     
-    @IBAction func PickAnImage(sender: AnyObject) {
+    override func viewDidLoad() {
+        
+        topTextField.hidden = true
+        bottomTextField.hidden = true
+        
+        // Set text field styling
+        let memeTextAttributes = [
+            NSForegroundColorAttributeName: UIColor.whiteColor(),
+            NSStrokeColorAttributeName: UIColor.blackColor(),
+            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName : NSNumber(double: -3.0)
+        ]
+        
+        // For text field delegate methods
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = .Center
+        
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.textAlignment = .Center
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // TODO: enable / disable camera button based on if the camera exists
+        // cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    }
+    
+    @IBAction func PickAnImageFromAlbum(sender: AnyObject) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         self.presentViewController(imagePicker, animated: true, completion: nil)
     }
-        
+    
+    @IBAction func pickAnImageFromCamera (sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
     // UI Image Picker Controller Delegate Methods
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
@@ -29,11 +70,27 @@ UINavigationControllerDelegate {
         imageViewPicker.image = pickedImage
         
         self.dismissViewControllerAnimated(true, completion: nil)
+        
+        topTextField.hidden = false
+        bottomTextField.hidden = false
     }
     
         func imagePickerControllerDidCancel(picker: UIImagePickerController) {
             self.dismissViewControllerAnimated(true, completion: nil)
         }
     
+    // UI Text Field Delegates
+   
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        topTextField.resignFirstResponder()
+        bottomTextField.resignFirstResponder()
+        return true
+    }
+    
+
 }
+
+
+
+
 
