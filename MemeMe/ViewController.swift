fe6010692ce4e043c6f9a7a069f80f5fceb2eeb1
Chelsea Green <chelseagreen@chelseas-mbp.gateway.pace.com>
2015-12-22 02:11:53 -0800

    add nav bar

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -15,6 +15,9 @@ UINavigationControllerDelegate, UITextFieldDelegate {
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var navBar: UIToolbar!
     var activeTextField = UITextField()
     
     override func viewDidLoad() {
@@ -39,6 +42,8 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         
         bottomTextField.defaultTextAttributes = memeTextAttributes
         bottomTextField.textAlignment = .Center
+        
+        shareButton.enabled = false
         
     }
     
@@ -76,21 +81,24 @@ UINavigationControllerDelegate, UITextFieldDelegate {
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
         let pickedImage : UIImage = image
+        
         imageViewPicker.contentMode = .ScaleAspectFit
+        //imageViewPicker.frame = CGRectMake(0, 0, 100, 200);
+    
         imageViewPicker.image = pickedImage
         
         self.dismissViewControllerAnimated(true, completion: nil)
         
         topTextField.hidden = false
         bottomTextField.hidden = false
+        shareButton.enabled = true 
     }
     
         func imagePickerControllerDidCancel(picker: UIImagePickerController) {
             self.dismissViewControllerAnimated(true, completion: nil)
         }
     
-    // UI Text Field Delegates
-    
+    // UI Text Field Delegate Methods
     func textFieldDidBeginEditing(textField: UITextField) {
         activeTextField = textField
     }
@@ -100,6 +108,7 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         return true
     }
     
+    //Keyboard Notifications
     func subscribeToKeyboardNotifications() {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:"    , name: UIKeyboardWillShowNotification, object: nil)
          NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:"    , name: UIKeyboardWillHideNotification, object: nil)
@@ -112,6 +121,7 @@ UINavigationControllerDelegate, UITextFieldDelegate {
             UIKeyboardWillHideNotification, object: nil)
     }
     
+    //Slide up view for bottom text field based on keyboard height
     func keyboardWillShow(notification: NSNotification) {
         if activeTextField == bottomTextField {
         view.frame.origin.y -= getKeyboardHeight(notification)
@@ -129,6 +139,42 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
         return keyboardSize.CGRectValue().height
     }
+    
+    func generateMemedImage() -> UIImage
+    {
+        toolBar.hidden = true
+        navBar.hidden = true
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        self.view.drawViewHierarchyInRect(self.view.frame,
+            afterScreenUpdates: true)
+        let memedImage : UIImage =
+        UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return memedImage
+    }
+    
+    //func save () {
+        //let meme = Meme( text: topTextField.text!, bottomTextField.text!, image: imageViewPicker.image, memedImage: memedImage)
+    //}
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
